7f363726209bdcd49d9bbf986ba45bb384c1ec42
Luis <aotourdeveloper1@gmail.com>
2024-11-14 12:48:04 -0500

    viajeController update

--- app/Http/Controllers/ViajeController.php
+++ app/Http/Controllers/ViajeController.php
@@ -312,7 +312,7 @@ class ViajeController extends Controller
             ];
 
             $fechaHoy = Carbon::now('America/Bogota')->format('Y-m-d');
-            $query .= " AND v.fecha_viaje " . ($request->fecha ? ("= " . $request->fecha) : (">= " . $fechaHoy));
+            $query .= " AND v.fecha_viaje " . ($request->fecha ? ("= '" . $request->fecha . "'") : (">= '" . $fechaHoy . "'"));
             // Comprobar si hay múltiples estados de viaje
             if (!isset($validatedData['estado_viaje']) || !empty($validatedData['estado_viaje'])) {
                 $placeholders = implode(',', array_fill(0, count($validatedData['estado_viaje']), '?'));
