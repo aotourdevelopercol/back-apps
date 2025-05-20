ff1ed9cb1b36d7bcff981a8f86b33a84fe34063f
aotourdeveloper1 <115663529+aotourdeveloper1@users.noreply.github.com>
2025-05-20 17:01:36 -0500

    asdfg

--- app/Mail/PagoProveedores.php
+++ app/Mail/PagoProveedores.php
@@ -23,26 +23,46 @@ class PagoProveedores extends Mailable implements ShouldQueue
      /**
       * Get the message envelope.
       */
-     public function envelope(): Envelope
-     {
+    //  public function envelope(): Envelope
+    //  {
         
-         return new Envelope(
-             subject: 'Pago proveedores',
-             from: new Address(config('mail.from.address'), config('mail.from.name'))
-         );
-     }
+    //      return new Envelope(
+    //          subject: 'Pago proveedores',
+    //          from: new Address(config('mail.from.address'), config('mail.from.name'))
+    //      );
+    //  }
  
      /**
       * Get the message content definition.
       */
-     public function content(): Content
-     {
-        \Log::info('Cargando correo PagoProveedores');
-         return new Content(
-             view: 'inscripcion_proveedores_emails.email_cuenta_de_cobro_corregir'
-        //     with: []
-         );
-     }
+    //  public function build(): 
+    //  {
+    //     \Log::info('Cargando correo PagoProveedores');
+    //      return new Content(
+    //          view: 'inscripcion_proveedores_emails.email_cuenta_de_cobro_corregir'
+    //     //     with: [] 
+    //      );
+    //  } proveedores_email\pago_proveedor.blade.php
+
+    public function build()
+    {
+        try {
+            return $this
+            ->from(config('mail.from.address'), config('mail.from.name'))
+                ->subject('Asignacion de viaje')
+                ->view('proveedores_email.pago_proveedor'); // Ensure the view name is correct
+        } catch (\Throwable $th) {
+            Log::error(
+                'Error al enviar el correo de pagos',
+                [
+                    'error' => $th->getMessage(),
+                ]
+            );
+
+            throw $th; // Optionally handle the exception further or return a user-friendly message
+        }
+    }
+
  
      /**
       * Get the attachments for the message.
