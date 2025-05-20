daae00e9361540d4278b9cc52bd1999a08546ac5
aotourdeveloper1 <115663529+aotourdeveloper1@users.noreply.github.com>
2025-05-20 11:38:38 -0500

    updated pagoProvedores

--- app/Mail/PagoProveedores.php
+++ app/Mail/PagoProveedores.php
@@ -39,7 +39,7 @@ class PagoProveedores extends Mailable
      public function content(): Content
      {
          return new Content(
-             view: 'provedores_email.pago_proveedorer'
+             view: 'provedores_email.pago_proveedor'
         //     with: []
          );
      }
